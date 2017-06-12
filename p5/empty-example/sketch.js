a33c75bc74d0b813aedf56ec075314a2cc1827c2
lastroundiniz-home <gustavodiniztargino@hotmail.com>
2017-06-12 00:07:46 -0300

    #1

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,73 @@
+var cols = 5;
+var rows = 5;
+var grid = new Array(cols);
+
+var openSet = [];
+var closedSet = [];
+var start;
+var end;
+var w, h;
+
+function Spot(i,j) {
+	this.x = i;
+	this.y = j;
+	this.f = 0;
+	this.g = 0;
+	this.h = 0;
+
+	this.show = function(col) {
+		fill(col);
+		noStroke();
+		rect(this.x*w, this.y*h, w-1, h-1);
+	}
+}
+
+function setup() {
+	createCanvas(400, 400);
+	console.log('A*');
+
+	w = width / cols;
+	h = height / rows;
+
+	//Fazendo um array 2d
+	for (var i = 0; i < cols; i++) {
+		grid[i] = new Array(rows);
+	}
+
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			grid[i][j] = new Spot(i,j);
+		}
+	}
+
+	start = grid[0][0];
+	end = grid[cols - 1][rows - 1];
+
+	openSet.push(start);
+
+	console.log(grid);
+}
+
+function draw() {
+	if (openSet.length > 0) {
+		//continua
+	} else {
+		//sem solucao
+	}
+	background(0);
+
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			grid[i][j].show(color(255));
+		}
+	}
+
+
+	for (var i = 0; i < closedSet.length; i++) {
+		closedSet[i].show(color(255,0,0));
+	}
+
+	for (var i = 0; i < openSet.length; i++) {
+		openSet[i].show(color(0,255,0));
+	}
+}
